159295dd2187568bd18935e439bb926cc8288d81
tatajae <shintaaaaagustinaaaa@gmail.com>
2026-05-23 18:34:22 +0800

    updatean

--- database/safepass.sql
+++ database/safepass.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2026 at 08:36 AM
+-- Generation Time: May 18, 2026 at 06:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -57,7 +57,15 @@ INSERT INTO `logs` (`id`, `user_id`, `activity`, `created_at`) VALUES
 (16, 26, 'login', '2026-05-15 12:31:45'),
 (17, 26, 'login', '2026-05-15 13:20:59'),
 (18, 26, 'login', '2026-05-16 05:59:41'),
-(19, 26, 'login', '2026-05-16 06:09:25');
+(19, 26, 'login', '2026-05-16 06:09:25'),
+(20, 26, 'login', '2026-05-18 02:48:57'),
+(21, 26, 'login', '2026-05-18 03:18:55'),
+(22, 26, 'login', '2026-05-18 03:37:26'),
+(23, 26, 'login', '2026-05-18 03:43:26'),
+(24, 26, 'login', '2026-05-18 03:50:12'),
+(25, 26, 'login', '2026-05-18 04:06:14'),
+(26, 26, 'login', '2026-05-18 04:15:43'),
+(27, 26, 'login', '2026-05-18 04:37:23');
 
 -- --------------------------------------------------------
 
@@ -91,6 +99,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `created_at`, `last_login`, `salt`, 
 CREATE TABLE `vaults` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
+  `website` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `password_strength` varchar(20) DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -103,9 +113,12 @@ CREATE TABLE `vaults` (
 -- Dumping data for table `vaults`
 --
 
-INSERT INTO `vaults` (`id`, `user_id`, `created_at`, `password_strength`, `updated_at`, `encrypted_data`, `iv`, `salt`) VALUES
-(18, 26, '2026-05-16 06:19:51', NULL, '2026-05-16 06:19:51', 'zxVF5RCI7oZCsZcQ82ay0/C2vL2MoQRk8S3vNzv9gPwN0JbEGTjKEZgESWv/Hb5tAuC4/wfbVXVXUxp4YqxIW+Vti/QS+bQFpPOXSoPoWrtRbdOPUWaTsPEfzrQfcpxq8S7E4AhwoY8Yt0+csQ==', 'UiKXfCmzfGTJBNAO', 'RQRY4k/dJGM3AN09p9fjZQ=='),
-(19, 26, '2026-05-16 06:30:39', NULL, '2026-05-16 06:30:39', 'AQ5YUiPLdILtcHCKfgXhyMElbQ+I28EoSNntg3ctdUrXqZIwsECvBLSIgSvyX8bea8vlIXRHgIIxnG34i7FwVpH+IVYf0u501AYiXgZis9TXvf/JLC7SxbiiY/975J3uc7drC3lix56v', 'eo7SnLeYnK0xx6Td', 'osyk2xo+TzVt91un6gkgcA==');
+INSERT INTO `vaults` (`id`, `user_id`, `website`, `username`, `created_at`, `password_strength`, `updated_at`, `encrypted_data`, `iv`, `salt`) VALUES
+(18, 26, '', '', '2026-05-16 06:19:51', NULL, '2026-05-16 06:19:51', 'zxVF5RCI7oZCsZcQ82ay0/C2vL2MoQRk8S3vNzv9gPwN0JbEGTjKEZgESWv/Hb5tAuC4/wfbVXVXUxp4YqxIW+Vti/QS+bQFpPOXSoPoWrtRbdOPUWaTsPEfzrQfcpxq8S7E4AhwoY8Yt0+csQ==', 'UiKXfCmzfGTJBNAO', 'RQRY4k/dJGM3AN09p9fjZQ=='),
+(19, 26, '', '', '2026-05-16 06:30:39', NULL, '2026-05-16 06:30:39', 'AQ5YUiPLdILtcHCKfgXhyMElbQ+I28EoSNntg3ctdUrXqZIwsECvBLSIgSvyX8bea8vlIXRHgIIxnG34i7FwVpH+IVYf0u501AYiXgZis9TXvf/JLC7SxbiiY/975J3uc7drC3lix56v', 'eo7SnLeYnK0xx6Td', 'osyk2xo+TzVt91un6gkgcA=='),
+(21, 26, 'Instagram', 'tata_gaming', '2026-05-18 03:54:45', NULL, '2026-05-18 04:22:59', 'UGRgs1wP6z84JrqIL7XFDVznyZ43MVT4GwK6hrcaLdpFpK+IlU32ZnjLAl3BOOhO/gvnXUiyZvG3Xi+c9tbz1OOBrykXpsWg0szm9AMBZ5Utriw7VgjLSMyBsCSWUNRATVc=', 'CFXDp5B09rkV6qIo', 'YfrKmEHB+ijtU5Z/qfXtHw=='),
+(22, 26, '', '', '2026-05-18 04:06:58', NULL, '2026-05-18 04:06:58', 'k4YMjY5t6bqF+M7TXody8aAnhp7JnFRrwqEMk6rDHUJJa5Sl6590vMe9z5zh+JMF+712LJCofJBktj4QnFki2uq62DT6dSRcUx6gccxMRbJ3raVZiNL5/anEb2S4bMXXdFRwFA==', 'beO51Zw+XE19uMkj', 'H4cAR0EyKaxuscIggWRGLw=='),
+(24, 26, '', '', '2026-05-18 04:27:46', NULL, '2026-05-18 04:27:46', '6QsPeKpgzOMPjNzqcLHEwaoJ7DDvEnBX18+6zGdYfnoi6laRAVnB/y29yrTmACU24Eh/PsKhF1CB6hQePYoLKzA3hlo5LExROFI17j8V7n86+SkdDJhdb9Y1tqEcJbrn', 'jlar5kr1AE6KqgOK', 'dKS9w/XEAYFHZF9O5YxrFA==');
 
 --
 -- Indexes for dumped tables
@@ -138,7 +151,7 @@ ALTER TABLE `vaults`
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -150,7 +163,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `vaults`
 --
 ALTER TABLE `vaults`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
